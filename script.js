90169f6f3720bcb8077173bf288785291bc150e0
waltidor-06 <82045347+waltidor-06@users.noreply.github.com>
2021-06-26 23:19:53 -0400

    Moves search box to be mobile responsive, and deletes unecessary psuedocode from scriptjs file.

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-
-
-//function initMap() {
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 33.7490, lng: -84.3880 },
-//     zoom: 8,
-//   });
-// }
-
 function initAutocomplete() {
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 33.7490, lng: -84.3880 },
